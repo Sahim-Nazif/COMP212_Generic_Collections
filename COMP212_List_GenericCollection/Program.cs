0d055a4f5967e1008ea2a6c4c87c00ff408c41ed
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-03-31 17:40:45 -0400

    Add project files.

---
+++ COMP212_List_GenericCollection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP212_List_GenericCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] movies = { "A Quite Place 2", "The Gurdge", "The" };
+
+            // TODO: Create the list
+
+
+            // TODO: Add some elements to the list from an existing array
+
+
+            // TODO: Add individual items
+
+
+            // TODO: Examine the Count and Capacity
+
+
+            // TODO: Enumerate the items in the list with a foreach loop
+
+
+            // TODO: Access any item by its index
+
+
+            // TODO: Remove items from the list
+
+
+            // TODO: Sort the list content
+
+
+            // ** Searching List Content **/
+
+            // TODO: Determine if a list contains a given item
+
+
+            // TODO: The Exists function gives us a more customizable way to search
+            // by using a predicate function to examine each element in the list
+
+
+            // TODO: Use the Find function to search through items
+
+
+            // TODO: The FindAll function can find multiple items
+
+
+            // TODO: Use TrueForAll to see if a given condition is true for all elements
+        }
+    }
+}
