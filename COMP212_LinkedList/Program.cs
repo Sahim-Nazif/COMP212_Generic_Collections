59c5a7e761df539e97fcb8b7629ff42112ef5e00
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-04-01 14:12:52 -0400

    created a string array

--- COMP212_LinkedList/Program.cs
+++ COMP212_LinkedList/Program.cs
@@ -10,6 +10,27 @@ namespace COMP212_LinkedList
     {
         static void Main(string[] args)
         {
+            string[] top_programming_languages2020 = { "JavaScript", "Java", "C#","C" , "C++" ,"PHP", "Swift", "Go"};
+
+            // TODO: Create a LinkedList that holds strings
+
+
+            // TODO: Items can be added at the front or back of the list
+
+
+            // TODO: Like other collections, a LinkedList can be iterated
+
+
+            // TODO: First and Last properties return LinkedListNodes
+
+
+            // TODO: Items can be added or removed relative to an existing item
+
+
+            // TODO: Search for items in the list with Contains and Find
+
+
+            // TODO: The list can then be traversed with those properties
         }
     }
 }
