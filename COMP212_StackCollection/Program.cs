521c362d39cfc20428b48c7324bd178c3534420b
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-04-21 10:47:24 -0400

    created stack and pushed three items

--- COMP212_StackCollection/Program.cs
+++ COMP212_StackCollection/Program.cs
@@ -11,22 +11,24 @@ namespace COMP212_StackCollection
         static void Main(string[] args)
         {
             // TODO: Create an empty stack
-          
+            Stack<string> movieStack = new Stack<string>();
 
             // TODO: The Push function pushes new elements onto the stack
+            movieStack.Push("A Quiet Place");
+            movieStack.Push("Bird Box");
+            movieStack.Push("Candyman");
 
-          
 
             // TODO: The Count property indicates how many elements are on the stack
-            
+
             // TODO: Stacks can be enumerated in place without changing the content
-            
+
 
             // TODO: The Peek function examines the current top element but does not remove it
-            
+
 
             // TODO: The Pop function removes an element from the top
-          
+
 
             // TODO: Search for an element using Contains
         }
