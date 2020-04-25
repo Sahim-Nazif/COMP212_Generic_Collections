89ba565c26e8b727823709abfc0ca97979c33767
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-04-25 14:07:01 -0400

    Using Contain() to search an item in Queue

--- COMP212_QueueCollection/Program.cs
+++ COMP212_QueueCollection/Program.cs
@@ -30,7 +30,7 @@ namespace COMP212_QueueCollection
             //Peek at the front of the Queue. Peek will return the item/object at the beginning of the Queue without removing it
             string firstItem = myFavGames.Peek();
             Console.WriteLine("The first item in the Queue is : {0}", firstItem);
-
+                     
             //Remove items from the front of the Queue
             firstItem = myFavGames.Dequeue();
             Console.WriteLine("Removing an item from the front : {0}", firstItem);
@@ -38,13 +38,15 @@ namespace COMP212_QueueCollection
             {
                 Console.WriteLine(item);
             }
+            //Search an item using Contain () will return true on the console
+            Console.WriteLine(myFavGames.Contains("Call of Duty"));
             //we can continue removing first item from the queue using dequeue, because Queue is based on first in first out method
 
             //lastly we can use Clear to remove all item from the Queue
-
             myFavGames.Clear();
-            Console.WriteLine("Number of items in Queue is :{0}", items);
-            Console.ReadLine();
+            Console.WriteLine("After clearing all the queue the count is: " + myFavGames.Count());
+
+            Console.ReadKey();
         }
     }
 }
