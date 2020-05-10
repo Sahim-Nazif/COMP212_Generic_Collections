33a58f0e358caec99f3bffc2002e70408e5680d3
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-05-10 16:51:28 -0400

    dictionary object created

--- COMP212_DictionaryCollections/Program.cs
+++ COMP212_DictionaryCollections/Program.cs
@@ -10,6 +10,18 @@ namespace COMP212_DictionaryCollections
     {
         static void Main(string[] args)
         {
+            //Main advantage of dictionary is that it's fast and unique key to value mapping
+            Dictionary <string ,string>myDictionary= new Dictionary<string, string>();
+
+            //Dictionaries map keys to values. Keys must unique
+
+            //add some keys and values to the dictoinary
+
+            //use the count property to see how many items there are.
+
+            //Trying to add an existing key will throw an exception
+
+            //fileTypes.Add
         }
     }
 }
